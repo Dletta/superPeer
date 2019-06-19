b0d67d2e23d76e138d7068366467149f01048517
Dletta <jcduschletta@gmail.com>
2019-06-18 20:02:44 -0700

    Added logging for In/Out, Gun Peers, Gun Graph

--- index.js
+++ index.js
@@ -13,6 +13,19 @@ const server = app.listen(port, host);
 
 console.log(`server listening on http://${host}:${port}`);
 
+function logIn(a,b,c,d){
+  console.log(`in a:${a}, b:${b}, c:${c}, d:${d}.........`);
+}
+
+function logOut(a,b,c,d){
+  console.log(`in a:${a}, b:${b}, c:${c}, d:${d}.........`);
+}
+
+Gun.on('in', logIn);
+
+Gun.on('out', logOut);
+
+
 var gun = Gun({
   web: server,
   localStorage: false,
@@ -23,7 +36,12 @@ function logPeers() {
   console.log(`Peers: ${Object.keys(gun._.opt.peers).join(', ')}`);
 }
 
+function logData() {
+  console.log(`In Memory: ${gun._.graph}`);
+}
+
 setInterval(logPeers, 5000); //Log peer list every 5 secs
+setInterval(logData, 20000); //Log gun graph every 20 secs
 
 
 const view = path.join(__dirname, 'view/main.html');
